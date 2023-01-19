9a6ab20003783f091f575c460a9e4dab642a5815
mirko <autieromir@gmail.com>
2023-01-19 04:41:31 +0100

    commit

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import "express-async-errors";
+import prisma from "./lib/prisma/client";
 
 const app = express();
+app.use(express.json());
 
 const hello = "ciao";
 console.log(hello);
