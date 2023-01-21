51dcb629ea670c255aab1e389f8eb1f7977da80f
mirko <autieromir@gmail.com>
2023-01-21 20:49:09 +0100

    es-3

--- src/app.ts
+++ src/app.ts
@@ -25,6 +25,38 @@ app.post(
     }
 );
 
+app.put(
+    "/fruits/id:(\\d+)",
+    validate({ body: fruitsSchema }),
+    async (request, response, next) => {
+        const fruitId = Number(request.params.id);
+        const fruitData: FruitsData = request.body;
+        try {
+            const fruit = await prisma.fruits.update({
+                where: { id: fruitId },
+                data: fruitData,
+            });
+            response.status(200).json(fruit);
+        } catch (error) {
+            response.status(404);
+            next(`cannot PUT /fruits/${fruitId}`);
+        }
+    }
+);
+
+app.delete("/fruits/id:(\\d+)", async (request, response, next) => {
+    const fruitId = Number(request.params.id);
+    try {
+        await prisma.fruits.delete({
+            where: { id: fruitId },
+        });
+        response.status(200).end();
+    } catch (error) {
+        response.status(404);
+        next(`cannot DELETE /fruits/${fruitId}`);
+    }
+});
+
 app.use(ValidationErrorMiddleware);
 
 export default app;
